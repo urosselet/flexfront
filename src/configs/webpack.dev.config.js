b93cce5aa78a1b805b6f00e844922b1cc6d2c3ee
David L <jean.legrand@he-arc.ch>
2017-11-14 11:14:58 +0100

    Update server config

--- src/configs/webpack.dev.config.js
+++ src/configs/webpack.dev.config.js
@@ -42,8 +42,8 @@ module.exports = webpackMerge(commonConfig, {
             'process.env': {
                 'ENV': JSON.stringify('DEV'),
                 'NODE_ENV': JSON.stringify('DEV'),
-                'API_URL': JSON.stringify('http://flexcrowd.org:8082/'),
-                'SOCKET_URL': JSON.stringify('http://flexcrowd.org:8081'),
+                'API_URL': JSON.stringify('http://api.flexcrowd.org:'),
+                'SOCKET_URL': JSON.stringify('http://dev.flexcrowd.org'),
             }
         }),
 
